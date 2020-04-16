72c7595d09b7f9f7398f18357afea7e7677a3a34
Claire Lally <clairelally08@gmail.com>
2020-04-16 20:01:54 +0000

    Linking DB to Python file & creation of env.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
-app = Flask(__name__)
+
+APP = Flask(__name__)
+
+# Defining variables database and MongoDB url
+APP.config["MONGO_DBNAME"] = os.getenv('MONGO_DBNAME')
+APP.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+MONGO = PyMongo(APP)
+
 
 
 @app.route('/')
