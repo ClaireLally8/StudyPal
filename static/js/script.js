cbc0027149b79e3b754dd0887f32ab9e90e405c8
Claire Lally <clairelally08@gmail.com>
2020-04-19 22:00:31 +0000

    Script to get date entry automatically set to 1 year

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,11 @@
 const hamburgerBtn = document.getElementById('hamburgerBtn');
-
-
 hamburgerBtn.addEventListener('click', () => {
   sideMenu.classList.toggle('open');
 })
+// default the dates to today and 12 months from now
+  let today = new Date();
+  $("#startDate").val(serializeDate(today));
+
+  let endDate = new Date();
+  endDate.setDate(today.getDate() + 365);
+  $("#endDate").val(serializeDate(endDate));
