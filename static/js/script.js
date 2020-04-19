60ebf467811593f1ec92dd82766473fea371997b
Claire Lally <clairelally08@gmail.com>
2020-04-19 22:37:49 +0000

    Automatically starts new schedule from date & ends 12 months later

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,11 @@ const hamburgerBtn = document.getElementById('hamburgerBtn');
 hamburgerBtn.addEventListener('click', () => {
   sideMenu.classList.toggle('open');
 })
+
+function serializeDate(date) {
+  // this YYYY-MM-DD format is the same serialization used in HTML date input values
+  return date.toISOString().split("T")[0];
+}
 // default the dates to today and 12 months from now
   let today = new Date();
   $("#startDate").val(serializeDate(today));
