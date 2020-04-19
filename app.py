bce4a5b11805552ca9bad45a2c7eae3f829e18b7
Claire Lally <clairelally08@gmail.com>
2020-04-19 00:27:13 +0000

    e-mail login auth. & session

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import os
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, session, request, redirect
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"):
     import env
-
 
 app = Flask(__name__)
 
@@ -15,13 +14,42 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
+# First page - index.html where user is asked to enter their email address to access their schedule
 @app.route('/')
-def get_homepage():
-    return render_template('index.html')
+def index():
+    if 'email' in session:
+        return 'You are logged in as ' + session['email']
 
+    return render_template('login.html')
 
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'email' : request.form['email']})
+
+    if login_user:
+            session['email'] = request.form['email']
+            return redirect(url_for('login'))
+
+    return 'email address not found! Try signing up!'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'email' : request.form['email']})
+
+        if existing_user is None:
+            users.insert({'email' : request.form['email']})
+            session['email'] = request.form['email']
+            return redirect(url_for('login'))
+        
+        return 'That email already exists!'
+
+    return render_template('register.html')
 
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
