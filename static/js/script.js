dbe57b9c423d23875f6bd072ed82a5f69d823fd1
Claire Lally <clairelally08@gmail.com>
2020-04-20 20:01:46 +0000

    Default to 1 year in dates

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,6 @@ function serializeDate(date) {
   // this YYYY-MM-DD format is the same serialization used in HTML date input values
   return date.toISOString().split("T")[0];
 }
-// default the dates to today and 12 months from now
   let today = new Date();
   $("#startDate").val(serializeDate(today));
 
