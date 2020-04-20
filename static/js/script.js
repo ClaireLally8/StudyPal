0be9e4941132e975f3cb3ee77b8288fe8161943e
Claire Lally <clairelally08@gmail.com>
2020-04-20 20:28:47 +0000

    calculate length of study plan

--- static/js/script.js
+++ static/js/script.js
@@ -7,9 +7,12 @@ function serializeDate(date) {
   // this YYYY-MM-DD format is the same serialization used in HTML date input values
   return date.toISOString().split("T")[0];
 }
-  let today = new Date();
+  let startDate = new Date();
   $("#startDate").val(serializeDate(today));
 
   let endDate = new Date();
   endDate.setDate(today.getDate() + 365);
   $("#endDate").val(serializeDate(endDate));
+
+  let scheduleLength = Math.round(endDate - startDate);
+  $("#scheduleLength").html(scheduleLength);
