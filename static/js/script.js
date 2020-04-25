167225fe478056e95f9867ed272a12b3ad96ea2c
Claire Lally <clairelally08@gmail.com>
2020-04-25 17:06:39 +0000

    removal of unneeded js

--- static/js/script.js
+++ static/js/script.js
@@ -3,21 +3,6 @@ hamburgerBtn.addEventListener('click', () => {
   sideMenu.classList.toggle('open');
 })
 
-function serializeDate(date) {
-  // this YYYY-MM-DD format is the same serialization used in HTML date input values
-  return date.toISOString().split("T")[0];
-}
-  let startDate = new Date();
-  $("#startDate").val(serializeDate(startDate));
-
-  let endDate = new Date();
-  endDate.setDate(startDate.getDate() + 365);
-  $("#endDate").val(serializeDate(endDate));
-
-  let milSec = 1000 * 60 * 60 * 24;
-  let scheduleLength = Math.round((endDate - startDate)/milSec);
-  $("#scheduleLength").html(scheduleLength);
-
   
 var acc = document.getElementsByClassName("accordion");
 var i;
